f6ba0213653a6fad37e3bbf6e3536efc03dc5ec3
nnalin04 <nnalin04@gmail.com>
2020-08-16 22:36:12 +0530

    Reading all the records at once into a list<String[]>

--- src/main/java/OpenCSVReader.java
+++ src/main/java/OpenCSVReader.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class OpenCSVReader {
 
@@ -13,12 +14,23 @@ public class OpenCSVReader {
             Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH));
             CSVReader csvReader = new CSVReader(reader)
         ) {
+            //Reading records one by one in a String array
             String[] nextRecord;
             while ((nextRecord = csvReader.readNext()) != null) {
                 System.out.println("Name : " + nextRecord[0]);
                 System.out.println("Email : " + nextRecord[1]);
                 System.out.println("Phone : " + nextRecord[2]);
                 System.out.println("Country : " + nextRecord[3]);
+                System.out.println("==========================");
+            }
+
+            //Reading records at once into a List<String[]>
+            List<String[]> records = csvReader.readAll();
+            for (String[] record : records){
+                System.out.println("Name : " + record[0]);
+                System.out.println("Email : " + record[1]);
+                System.out.println("Phone : " + record[2]);
+                System.out.println("Country : " + record[3]);
                 System.out.println("==========================");
             }
 
